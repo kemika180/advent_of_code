c329111ba1b0e87c0b000fb37ecd9ce93e957f94
kemika180 <jkgurchiek@gmail.com>
2025-12-02 00:10:03 -0700

    day2 part2 done

--- 2025/day02/day02.py
+++ 2025/day02/day02.py
@@ -1,5 +1,5 @@
 from sys import argv
-from typing import List, Tuple
+from typing import List, Set
 from collections import Counter
 
 # https://adventofcode.com/2025/day/2
@@ -22,7 +22,7 @@ def parse_data(data: List[str]) -> List[tuple]:
     return parsed_data
 
 
-def process_ranges(ranges: List[tuple]) -> List[int]:
+def process_ranges1(ranges: List[tuple]) -> List[int]:
     invalid_ids = []
     for low, high in ranges:
         for num in range(int(low), int(high)+1):
@@ -32,10 +32,32 @@ def process_ranges(ranges: List[tuple]) -> List[int]:
                 continue
             if number[length // 2:] != number[:length // 2]:
                 continue
-            print(num)
+            # print(num)
             invalid_ids.append(num)
 
     return invalid_ids
+
+
+def process_ranges2(ranges: List[tuple]) -> Set[int]:
+    invalid_ids = set()
+    for low, high in ranges:
+        for num in range(int(low), int(high)+1):
+            number = str(num)
+            length = len(number)
+            for n in range(2, length+1):
+                if length % n != 0:
+                    continue
+                l = length // n
+                chunks = [number[i:i+l] for i in range(0, length, l)]
+                # print(f"{n=} {chunks=}")
+                if len(Counter(chunks)) != 1:
+                    continue
+                if num not in invalid_ids:
+                    # print(num)
+                    invalid_ids.add(num)
+
+    return invalid_ids
+
 
 def main():
     if len(argv) < 2:
@@ -44,9 +66,10 @@ def main():
         filename = argv[1]
     data = read_file(filename)
     ranges = parse_data(data)
-    ids = process_ranges(ranges)
-
-    print(f"Total of invalid IDs is {sum(ids)}.")
+    ids1 = process_ranges1(ranges)
+    print(f"Part 1:\n  Total of invalid IDs is {sum(ids1)}.")
+    ids2 = process_ranges2(ranges)
+    print(f"Part 2:\n  Total of invalid IDs is {sum(ids2)}.")
 
 
 if __name__ == '__main__':
