d2423c52df4868bcefe0294519e2b57e26f47575
kemika180 <jkgurchiek@gmail.com>
2025-12-06 01:00:19 -0700

    day6 part2 done

--- 2025/day06/day06.py
+++ 2025/day06/day06.py
@@ -2,9 +2,8 @@ from sys import argv
 from aoc_logging import start_logging
 from pytictoc import TicToc
 import numpy as np
-import numpy.typing as npt
 
-def read_file1(filename: str) -> npt.NDArray:
+def read_file1(filename: str) -> list:
     data_list = []
     with open(filename) as file:
         try:
@@ -18,9 +17,48 @@ def read_file1(filename: str) -> npt.NDArray:
 
     data_array = np.array(data_list)
     data_array = np.rot90(data_array, k=3)
-    return data_array
 
-def process_data(data: npt.NDArray) -> int:
+    return list(data_array)
+
+def read_file2(filename: str) -> list:
+    data_list = []
+    number_list = []
+    symbol_list = []
+    with open(filename) as file:
+        try:
+            logger.info(f"opening {filename}")
+            for line in file:
+                line = list(line.rstrip('\n'))
+                data_list.append(line)
+            if filename == "example.txt":
+                symbol_list = ''.join(data_list[3]).split()
+                data_list = data_list[:3]
+            else:
+                symbol_list = ''.join(data_list[4]).split()
+                data_list = data_list[:4]
+
+        except FileNotFoundError:
+            logger.error("file not found")
+            raise FileNotFoundError("file not found")
+
+    data_list = list(np.rot90(np.array(data_list), k=1))
+
+    temp_list = []
+    temp_list.append(symbol_list.pop())
+    for i in range(0, len(data_list)):
+        line = "".join(data_list[i]).strip()
+        if line == "":
+            number_list.append(temp_list.copy())
+            temp_list = []
+            temp_list.append(symbol_list.pop())
+        else:
+            temp_list.append(int(line))
+    else:
+        number_list.append(temp_list.copy())
+
+    return number_list
+
+def process_data(data: list) -> int:
     total = 0
     for line in data:
         logger.info(line)
@@ -46,10 +84,15 @@ def main():
         filename = argv[1]
     t = TicToc()
 
-    data = read_file1(filename)
     t.tic()
+    data = read_file1(filename)
     total = process_data(data)
-    print(f"Part 1:\n  Grand Total = {total}")
+    logger.info(f"Part 1:\n  Grand Total = {total}")
+    t.toc()
+    t.tic()
+    data2 = read_file2(filename)
+    total = process_data(data2)
+    logger.info(f"Part 2:\n  Grand Total = {total}")
     t.toc()
 
 if __name__ == '__main__':
