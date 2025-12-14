f8b141bcf661a4c5b86f6c1e7a0264fe7d431f5c
kemika180 <jkgurchiek@gmail.com>
2025-12-14 11:36:03 -0700

    day9 part2 done

--- 2025/day09/day09.py
+++ 2025/day09/day09.py
@@ -1,6 +1,7 @@
 from sys import argv
 from aoc_logging import start_logging
 from pytictoc import TicToc
+from shapely import Polygon, box
 
 def read_file(filename: str) -> list:
     data_list = []
@@ -24,6 +25,23 @@ def process_data(data: list) -> int:
             largest = max([largest, area])
     return largest
 
+def check_polygons(data: list) -> int:
+    largest = 0
+
+    points = [(int(a), int(b)) for a, b in data]
+
+    shape1 = Polygon(points)
+    for i, point1 in enumerate(points):
+        for point2 in points[i+1:]:
+            x1, y1 = point1
+            x2, y2 = point2
+            shape2 = box(min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2))
+            if shape1.contains(shape2):
+                area = (abs(int(x2)-int(x1))+1)*(abs(int(y2)-int(y1))+1)
+                largest = max([largest, area])
+
+    return largest
+
 def main():
     if len(argv) == 1:
         filename = "example.txt"
@@ -35,6 +53,10 @@ def main():
     t.tic()
     output = process_data(data)
     logger.info(f"Part 1:\n  Largest area is {output}")
+    t.toc()
+    t.tic()
+    output2 = check_polygons(data)
+    logger.info(f"Part 2:\n  Largest area is {output2}")
     t.toc()
 
 if __name__ == '__main__':
